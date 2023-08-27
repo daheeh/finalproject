cee67a00b73979fa1cc6f7d174ae901811801b60
{daheeh} <odaheei@gmail.com>
2023-08-27 17:42:09 +0900

    STOMP단일서버에서 Redis pub/sub 다중서버로 수정

--- src/main/java/com/highright/chattingtest/controller/ChatController.java
+++ src/main/java/com/highright/chattingtest/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.highright.chattingtest.controller;
 
 import com.highright.chattingtest.model.dto.ChatDTO;
 import com.highright.chattingtest.model.dto.ChatRoomDTO;
+import com.highright.chattingtest.pubsub.RedisPublisher;
+import com.highright.chattingtest.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -14,12 +16,19 @@ import java.util.List;
 @Controller
 public class ChatController {
 
-    private final SimpMessageSendingOperations messagingTemplate;
+    private final RedisPublisher redisPublisher;
+    private final ChatRoomRepository chatRoomRepository;
 
-    @MessageMapping(value = "/chat/message")
-    public void message(ChatDTO message){
-        if(ChatDTO.MessageType.ENTER.equals(message.getType()))
+    /**
+     * websocket "/pub/chat/message"로 들어오는 메시징을 처리한다.
+     */
+    @MessageMapping("/chat/message")
+    public void message(ChatDTO message) {
+        if (ChatDTO.MessageType.ENTER.equals(message.getType())) {
+            chatRoomRepository.enterChatRoom(message.getRoomId());
             message.setMessage(message.getSender() + "님이 입장하셨습니다.");
-        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getRoomId(), message);
+        }
+        // Websocket에 발행된 메시지를 redis로 발행한다(publish)
+        redisPublisher.publish(chatRoomRepository.getTopic(message.getRoomId()), message);
     }
 }
--- src/main/java/com/highright/chattingtest/model/dto/ChatRoomDTO.java
+++ src/main/java/com/highright/chattingtest/model/dto/ChatRoomDTO.java
@@ -3,6 +3,7 @@ package com.highright.chattingtest.model.dto;
 import lombok.*;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
@@ -11,7 +12,9 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Getter
 @Setter
-public class ChatRoomDTO {
+public class ChatRoomDTO implements Serializable {
+
+    private static final long serialVersionUID = 6494678977089006638L;
 
     private String roomId;
     private String name;
